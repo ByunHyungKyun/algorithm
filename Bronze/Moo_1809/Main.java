cdafd1db9b5ef9f9de3d6951fe0000f39debf0e1
byun <princebyun@gmail.com>
2024-02-23 11:58:37 +0900

    백준 1809 문제 해결

---
+++ Bronze/Moo_1809/Main.java
@@ -0,0 +1,18 @@
+package Bronze.Moo_1809;
+
+class Main {
+    public static void main(String[] args)  {
+
+/**
+ * You’ve decided to buy a farm and start a new life. To pass some time while you wait for the title of the land to go through, produce the following ascii cow.
+ */
+
+
+        System.out.println("\"(___)\n" +
+                "(o o)____/\n" +
+                " @@      \\\\\n" +
+                "  \\\\ ____,/\n" +
+                "  //   //\n" +
+                " ^^   ^^\"");
+    }
+}
